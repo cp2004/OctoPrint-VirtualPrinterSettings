44f797a21f156c9ba19eee3ffa7a0352a2bab295
cp2004 <31997505+cp2004@users.noreply.github.com>
2020-07-30 22:48:27 +0100

    :art: Convert indentation to spaces

--- octoprint_virtual_printer_settings/__init__.py
+++ octoprint_virtual_printer_settings/__init__.py
@@ -5,46 +5,46 @@ import octoprint.plugin
 
 
 class VirtualPrinterSettingsPlugin(octoprint.plugin.TemplatePlugin,
-								   octoprint.plugin.RestartNeedingPlugin):
+                                   octoprint.plugin.RestartNeedingPlugin):
 
-	def get_template_configs(self):
-		return [dict(
-			type="settings",
-			name="Virtual Printer",
-			replaces="plugin_virtual_printer",
-			template="virtual_printer2_settings.jinja2",
-			custom_bindings=False
-		)]
+    def get_template_configs(self):
+        return [dict(
+            type="settings",
+            name="Virtual Printer",
+            replaces="plugin_virtual_printer",
+            template="virtual_printer2_settings.jinja2",
+            custom_bindings=False
+        )]
 
-	##~~ Softwareupdate hook
+    # Softwareupdate hook
+    def get_update_information(self):
+        return dict(
+            virtual_printer2=dict(
+                displayName="Virtual Printer Settings",
+                displayVersion=self._plugin_version,
 
-	def get_update_information(self):
-		return dict(
-			virtual_printer2=dict(
-				displayName="Virtual Printer Settings",
-				displayVersion=self._plugin_version,
+                # version check: github repository
+                type="github_release",
+                user="cp2004",
+                repo="OctoPrint-VirtualPrinterSettings",
+                current=self._plugin_version,
 
-				# version check: github repository
-				type="github_release",
-				user="cp2004",
-				repo="OctoPrint-VirtualPrinterSettings",
-				current=self._plugin_version,
-
-				# update method: pip
-				pip="https://github.com/cp2004/OctoPrint-VirtualPrinterSettings/archive/{target_version}.zip"
-			)
-		)
+                # update method: pip
+                pip="https://github.com/cp2004/OctoPrint-VirtualPrinterSettings/archive/{target_version}.zip"
+            )
+        )
 
 
 __plugin_name__ = "Virtual Printer Settings"
-__plugin_pythoncompat__ = ">=2.7,<4" # python 2 and 3
+__plugin_pythoncompat__ = ">=2.7,<4"  # python 2 and 3
+
 
 def __plugin_load__():
-	global __plugin_implementation__
-	__plugin_implementation__ = VirtualPrinterSettingsPlugin()
+    global __plugin_implementation__
+    __plugin_implementation__ = VirtualPrinterSettingsPlugin()
 
-	global __plugin_hooks__
-	__plugin_hooks__ = {
-		"octoprint.plugin.softwareupdate.check_config": __plugin_implementation__.get_update_information
-	}
+    global __plugin_hooks__
+    __plugin_hooks__ = {
+        "octoprint.plugin.softwareupdate.check_config": __plugin_implementation__.get_update_information
+    }
 
