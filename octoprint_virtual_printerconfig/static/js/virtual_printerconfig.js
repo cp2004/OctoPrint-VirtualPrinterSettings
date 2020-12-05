73b2a121df9ccc02c0fb3e3feac54ba7b3253d35
cp2004 <31997505+cp2004@users.noreply.github.com>
2020-12-05 17:43:20 +0000

    :sparkles: Add OctoPrint 1.5.0 extra settings
    closes #6

--- octoprint_virtual_printerconfig/static/js/virtual_printerconfig.js
+++ octoprint_virtual_printerconfig/static/js/virtual_printerconfig.js
@@ -9,6 +9,14 @@ $(function() {
         var self = this;
 
         self.settingsViewModel = parameters[0]
+
+        self.resend_every_n = ko.pureComputed(function (){
+            if (self.settingsViewModel.settings.plugins.virtual_printer.resend_ratio() > 0) {
+                return Math.floor(100 / self.settingsViewModel.settings.plugins.virtual_printer.resend_ratio())
+            } else {
+                return 0
+            }
+        })
     }
     OCTOPRINT_VIEWMODELS.push({
         construct: Virtual_Printer_SettingsViewModel,
