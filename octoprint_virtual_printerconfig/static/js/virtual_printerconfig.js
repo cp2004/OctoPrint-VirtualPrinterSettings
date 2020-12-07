54e164e9fcac074403af864a22ae7f98727a6ff1
cp2004 <31997505+cp2004@users.noreply.github.com>
2020-12-07 22:13:00 +0000

    :sparkles: Enable editing the reset lines & prepared OKs

--- octoprint_virtual_printerconfig/static/js/virtual_printerconfig.js
+++ octoprint_virtual_printerconfig/static/js/virtual_printerconfig.js
@@ -26,6 +26,10 @@ $(function () {
 
         self.capabilities = ko.observableArray([]);
         self.newCapability = ko.observable();
+        self.resetLines = ko.observableArray([]);
+        self.newResetLine = ko.observable();
+        self.preparedOks = ko.observableArray([]);
+        self.newPreparedOk = ko.observable();
 
         self.addCapability = function () {
             self.capabilities.unshift({
@@ -34,9 +38,25 @@ $(function () {
             });
             self.newCapability("");
         };
-
         self.deleteCapability = function (cap) {
             self.capabilities.remove(cap);
+        };
+
+        self.addResetLine = function () {
+            self.resetLines.unshift(ko.observable(self.newResetLine()));
+            self.newResetLine("");
+        };
+        self.deleteResetLine = function (cap) {
+            self.resetLines.remove(cap);
+        };
+
+        self.addPreparedOk = function () {
+            self.preparedOks.unshift(ko.observable(self.newPreparedOk()));
+            self.newPreparedOk("");
+        };
+
+        self.deletePreparedOk = function (cap) {
+            self.preparedOks.remove(cap);
         };
 
         self.onAllBound = self.onEventSettingsUpdated = self.onServerReconnect = function () {
@@ -51,6 +71,20 @@ $(function () {
                     value: ko.observable(value),
                 });
             }
+            self.resetLines([]);
+            for (let line in self.settingsViewModel.settings.plugins.virtual_printer.resetLines()) {
+                let value = self.settingsViewModel.settings.plugins.virtual_printer.resetLines()[
+                    line
+                ];
+                self.resetLines.unshift(ko.observable(value));
+            }
+            self.preparedOks([]);
+            for (let line in self.settingsViewModel.settings.plugins.virtual_printer.preparedOks()) {
+                let value = self.settingsViewModel.settings.plugins.virtual_printer.preparedOks()[
+                    line
+                ];
+                self.preparedOks.unshift(ko.observable(value));
+            }
         };
 
         self.onSettingsBeforeSave = function () {
@@ -61,6 +95,24 @@ $(function () {
                 ].value;
             }
             self.settingsViewModel.settings.plugins.virtual_printer.capabilities = data;
+
+            self.settingsViewModel.settings.plugins.virtual_printer.resetLines(
+                []
+            );
+            for (let line in self.resetLines()) {
+                self.settingsViewModel.settings.plugins.virtual_printer.resetLines.push(
+                    self.resetLines()[line]
+                );
+            }
+
+            self.settingsViewModel.settings.plugins.virtual_printer.preparedOks(
+                []
+            );
+            for (let line in self.preparedOks()) {
+                self.settingsViewModel.settings.plugins.virtual_printer.preparedOks.push(
+                    self.preparedOks()[line]
+                );
+            }
         };
     }
     OCTOPRINT_VIEWMODELS.push({
